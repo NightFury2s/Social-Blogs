fa5039ce62a2c59acaf54eee2bd7663a8376ea08
ManhDang <89623168+NightFury2s@users.noreply.github.com>
2023-05-25 04:26:36 +0700

    init Social-Blogs

---
+++ social_blogs/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('blogs/', blogs, name='blogs'),
+    path('category_blogs/<str:slug>/', category_blogs, name='category_blogs'),
+    path('tag_blogs/<str:slug>/', tag_blogs, name='tag_blogs'),
+    path('blog/<str:slug>/', blog_details, name='blog_details'),
+]
---
+++ social_blogs/blog/views.py
@@ -0,0 +1,98 @@
+from django.shortcuts import get_object_or_404, redirect, render
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
+from .models import(
+    Blog,
+    Category,
+    Tag,
+)
+
+# Create your views here.
+
+def home(request):
+    blogs = Blog.objects.order_by('-created_date')
+    tags = Tag.objects.order_by('-created_date')
+    context = {
+        "blogs": blogs,
+        "tags": tags
+    }
+    return render(request, 'home.html', context)
+
+def blogs(request):
+    queryset = Blog.objects.order_by('-created_date')
+    tags = Tag.objects.order_by('-created_date')
+    page = request.GET.get('page', 1)
+    paginator = Paginator(queryset, 4)
+    
+    try:
+        blogs = paginator.page(page)
+    except EmptyPage:
+        blogs = paginator.page(1)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+        return redirect('blogs')
+    
+    context = {
+        "blogs": blogs,
+        "tags": tags,
+        "paginator": paginator
+    }
+    return render(request, 'blogs.html', context)
+
+
+def category_blogs(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    queryset = category.category_blogs.all()
+    tags = Tag.objects.order_by('-created_date')[:5]
+    page = request.GET.get('page', 1)
+    paginator = Paginator(queryset, 2)
+    all_blogs = Blog.objects.order_by('-created_date')[:5]
+    
+    try:
+        blogs = paginator.page(page)
+    except EmptyPage:
+        blogs = paginator.page(1)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+        return redirect('blogs')
+
+    context = {
+        "blogs": blogs,
+        "tags": tags,
+        "all_blogs": all_blogs
+    }
+    return render(request, 'category_blogs.html', context)
+
+def tag_blogs(request, slug):
+    tag = get_object_or_404(Tag, slug=slug)
+    queryset = tag.tag_blogs.all()
+    tags = Tag.objects.order_by('-created_date')[:5]
+    page = request.GET.get('page', 1)
+    paginator = Paginator(queryset, 2)
+    all_blogs = Blog.objects.order_by('-created_date')[:5]
+    
+    try:
+        blogs = paginator.page(page)
+    except EmptyPage:
+        blogs = paginator.page(1)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+        return redirect('blogs')
+
+    context = {
+        "blogs": blogs,
+        "tags": tags,
+        "all_blogs": all_blogs
+    }
+    return render(request, 'category_blogs.html', context)
+
+def blog_details(request, slug):
+    blog = get_object_or_404(Blog, slug=slug)
+    category = Category.objects.get(id=blog.category.id)
+    related_blogs = category.category_blogs.all()
+    tags = Tag.objects.order_by('-created_date')[:5]
+    context = {
+        "blog": blog,
+        "related_blogs" : related_blogs,
+        "tags": tags,
+    }
+    return render(request, 'blog_details.html', context)
