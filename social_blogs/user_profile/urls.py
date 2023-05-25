ff2f720ea4b24472b540de372a36671f755bd04a
ManhDang <89623168+NightFury2s@users.noreply.github.com>
2023-05-26 03:37:06 +0700

    Login & Signup

--- social_blogs/user_profile/urls.py
+++ social_blogs/user_profile/urls.py
@@ -3,5 +3,7 @@ from user_profile.views import login_user
 from .views import *
 
 urlpatterns = [
-    path('login/', login_user, name='login')
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout_user'),
+    path('register_user/', register_user, name='register_user')
 ]
--- social_blogs/user_profile/views.py
+++ social_blogs/user_profile/views.py
@@ -1,6 +1,51 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, logout, authenticate
+from .forms import (
+    UserRegistrationForm,
+    LoginForm,
+)
 
 # Create your views here.
 
 def login_user(request):
-    return render(request, 'login.html')
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data.get('username'),
+                password=form.cleaned_data.get('password')
+            )
+            if user:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.warning(request, "Sai username hoặc password")
+
+    context = {
+        "form": form
+    }
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+def register_user(request):
+    form = UserRegistrationForm()
+
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data.get('password'))
+            user.save()
+            messages.success(request, "Đăng ký thành công")
+            return redirect('login')
+
+    context = {
+        "form": form
+    }
+    return render(request, 'registration.html', context)
